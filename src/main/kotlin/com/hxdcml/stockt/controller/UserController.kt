a031e4c9ff2b91b0d2e1358e18fd1065fc53bc76
Souleiman Ayoub <souleiman@hxdcml.com>
2020-05-22 16:31:19 -0400

    Remove unused imports

--- src/main/kotlin/com/hxdcml/stockt/controller/UserController.kt
+++ src/main/kotlin/com/hxdcml/stockt/controller/UserController.kt
@@ -4,8 +4,6 @@ import com.hxdcml.stockt.model.request.CreateUserRequest
 import com.hxdcml.stockt.model.response.CreateUserResponse
 import mu.KotlinLogging
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
